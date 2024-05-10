931f831049a923fc464493aa1b6e3654be840356
Gui_S.B <141243910+Guilherme-SB-DEV@users.noreply.github.com>
2024-05-10 19:54:28 -0300

    Main.java

--- TrabalhoDeLucas/src/main/Main.java
+++ TrabalhoDeLucas/src/main/Main.java
@@ -1,13 +1,28 @@
 package main;
-import sql.DAO;
 import static sql.DAO.consulta;
+import static sql.DAO.insert;
+import static sql.DAO.update;
+import static sql.DAO.delete;
 import users.Usr;
 public class Main extends Usr{
     public static void main(String[] args){
         // TODO code application logic here
+        int counter = 0;
         Usr usuario = new Usr();
-        System.out.println(consulta());
-        consulta();
+        update(1, "nome", "nome_teste");
+        delete(2);
+        for(Usr usr: consulta()){
+            System.out.println(consulta().get(counter).getNome());
+            System.out.println(consulta().get(counter).getCidade());
+            System.out.println(consulta().get(counter).getBairro());
+            System.out.println(consulta().get(counter).getCidade());
+            System.out.println(consulta().get(counter).getCpf());
+            System.out.println(consulta().get(counter).getDdd());
+            System.out.println(consulta().get(counter).getNumero());
+            counter++;
+        }
+        
+        insert("gui", "cidade de sla", "bairro da palma", "71", "numero", "666.666.666-66");
     }
     
 }
