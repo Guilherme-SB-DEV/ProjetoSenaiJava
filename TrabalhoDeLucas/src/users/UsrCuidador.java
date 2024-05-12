41a458d5199d89347b4233ddf3e6b680d83a60f3
Gui_S.B <141243910+Guilherme-SB-DEV@users.noreply.github.com>
2024-05-11 22:26:42 -0300

    UsrCuidador.java

--- TrabalhoDeLucas/src/users/UsrCuidador.java
+++ TrabalhoDeLucas/src/users/UsrCuidador.java
@@ -1,13 +1,13 @@
 package users;
 import java.util.Date;
 public class UsrCuidador extends Usr{
-	private enum tipoContrato{
-		clt,
-		autonomo,
-	}
+        private String TipoContrato;
 	private String descricaoEspecialidade;
 	private String dddEmergencial;
 	private String numeroEmergencial;
+        private String periodo;
+        private String senha;
+        
 	
 	public String getDescricaoEspecialidade() {
 		return descricaoEspecialidade;
@@ -27,6 +27,43 @@ public class UsrCuidador extends Usr{
 	public void setNumeroEmergencial(String numeroEmergencial) {
 		this.numeroEmergencial = numeroEmergencial;
 	}
+
+
+        public String getPeriodo() {
+            return periodo;
+        }
+
+        public void setPeriodo(String periodo) {
+            this.periodo = periodo;
+        }
+
+    /**
+     * @return the TipoContrato
+     */
+    public String getTipoContrato() {
+        return TipoContrato;
+    }
+
+    /**
+     * @param TipoContrato the TipoContrato to set
+     */
+    public void setTipoContrato(String TipoContrato) {
+        this.TipoContrato = TipoContrato;
+    }
+
+    /**
+     * @return the senha
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 	
 	
 	
